e3d4a4de94e3b9d7d2812563683d7363e585c6e6
369webforall <369webforall@gmail.com>
2024-08-22 14:53:27 +0545

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Github action</h1>
+      <h2>1. Adding action for pullrequest</h2>
+    </div>
+  );
+};
+
+export default Home;
